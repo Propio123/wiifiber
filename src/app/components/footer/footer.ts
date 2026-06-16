a13b576c3cb5f62e544ba2153f44632719306ec9
Propio123 <juanchavez123@hotmail.es>
2026-06-16 11:47:17 -0500

    footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  isMenuOpen = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu() { 
+    this.isMenuOpen = !this.isMenuOpen; 
+  }
+
+  // Para secciones DENTRO de la misma página (Home)
+  scrollTo(elementId: string) {
+    this.isMenuOpen = false; 
+    
+    // Si estamos en otra página (ej: reporte), volvemos al home primero
+    if (this.router.url !== '/') {
+      this.router.navigate(['/']).then(() => {
+        setTimeout(() => this.performScroll(elementId), 300);
+      });
+    } else {
+      this.performScroll(elementId);
+    }
+  }
+
+  private performScroll(id: string) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+}
