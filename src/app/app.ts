b37bcc4fac50840256e12fc5d8dcc2b9409d4cae
Propio123 <juanchavez123@hotmail.es>
2026-06-18 10:49:41 -0500

    publicidad

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { Footer } from "./components/footer/footer";
+import { SocialSidebar } from "./components/social-sidebar/social-sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, SocialSidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/social-sidebar/social-sidebar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-sidebar',
+  imports: [],
+  templateUrl: './social-sidebar.html',
+  styleUrl: './social-sidebar.css',
+})
+export class SocialSidebar {}
